05d6c5d5508b0164c517e6dd0194ab56b326f071
bezhan <shadow_bez@yahoo.com>
2020-05-04 01:40:17 +0300

    ML Kit barcode reader added

--- app/src/main/java/com/example/catalogshopping/view/MainActivity.java
+++ app/src/main/java/com/example/catalogshopping/view/MainActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
+import android.view.PixelCopy;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.catalogshopping.CrosshairDrawable;
 import com.example.catalogshopping.ModelLoader;
@@ -18,11 +26,19 @@ import com.example.catalogshopping.model.ShoppingCart;
 import com.example.catalogshopping.other.FirebaseStorageConstants;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.core.Trackable;
 import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -45,6 +61,14 @@ public class MainActivity extends AppCompatActivity {
     private ModelLoader modelLoader;
 
     private StorageReference storageReference;
+
+    private FirebaseVisionBarcodeDetector detector;
+    private FirebaseVisionBarcodeDetectorOptions options =
+            new FirebaseVisionBarcodeDetectorOptions.Builder()
+                    .setBarcodeFormats(
+                            FirebaseVisionBarcode.FORMAT_EAN_13)
+                    .build();
+
 
     private Button dw;
 
@@ -87,6 +111,10 @@ public class MainActivity extends AppCompatActivity {
         // FIREBASE STORAGE
         //        FirebaseApp.initializeApp(this);
         storageReference = FirebaseStorage.getInstance().getReference();
+
+        // COMPUTER VISION ML
+        detector = FirebaseVision.getInstance()
+                .getVisionBarcodeDetector(options);
     }
 
     private void initUI() {
@@ -183,6 +211,52 @@ public class MainActivity extends AppCompatActivity {
     private android.graphics.Point getScreenCenter() {
         View view = findViewById(android.R.id.content);
         return new android.graphics.Point(view.getWidth() / 2, view.getHeight() / 2);
+    }
+
+
+
+    private void photoAction() {
+        ArSceneView view = fragment.getArSceneView();
+
+        // Create a bitmap the size of the scene view.
+        final Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        // Create a handler thread to offload the processing of the image.
+        final HandlerThread handlerThread = new HandlerThread("PixelCopier");
+        handlerThread.start();
+        // Make the request to copy.
+        PixelCopy.request(view, bitmap, (copyResult) -> {
+            if (copyResult == PixelCopy.SUCCESS) {
+                takePhotoFirebase(bitmap);
+            } else {
+                Toast toast = Toast.makeText(MainActivity.this,
+                        "Failed to copyPixels: " + copyResult, Toast.LENGTH_LONG);
+                toast.show();
+            }
+            handlerThread.quitSafely();
+        }, new Handler(handlerThread.getLooper()));
+
+    }
+
+    private void takePhotoFirebase(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
+                .addOnSuccessListener(barcodes -> {
+                    for (FirebaseVisionBarcode barcode: barcodes) {
+                        Rect bounds = barcode.getBoundingBox();
+                        Point[] corners = barcode.getCornerPoints();
+
+                        String rawValue = barcode.getRawValue();
+
+                        Snackbar.make(findViewById(android.R.id.content),
+                                rawValue, Snackbar.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(e -> {
+
+                });
     }
 
 }
